4f036f42ffa48a402f3feff1a673c043633c7886
chris22763 <cmuehrke@me.com>
2017-03-22 13:16:09 +0100

    Update GUI_WorkedOnce.java

--- JAVA_PROJEKT/TESTING_AREA/GUI_WorkedOnce.java
+++ JAVA_PROJEKT/TESTING_AREA/GUI_WorkedOnce.java
@@ -8,9 +8,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
